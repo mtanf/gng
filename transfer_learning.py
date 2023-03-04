c1baafd95830b440b46ef77d9c6f51fd0b08980a
fhgre <e.cero.931@gmail.com>
2023-03-04 16:56:11 +0100

    Modified transfer learning script
    Now it can train multiple instances of model without requesting more memory than available

--- transfer_learning.py
+++ transfer_learning.py
@@ -12,6 +12,7 @@ import json
 import tensorflow as tf
 from tensorflow import keras
 from tqdm import tqdm
+import gc
 
 def load_matrices(folder_path, input_label):
     reloaded_matrices = []
@@ -49,58 +50,71 @@ training_labels =np.asarray([y for x in [real_labels, generated_labels] for y in
 #get input shape
 input_shape = real_cores[0].shape
 
-#instantiate a base model with pre-trained weight
-base_model = keras.applications.MobileNetV3Large(input_shape = input_shape,
-                                                 weights="imagenet",
-                                                 include_top=False, #non include l'MLP con cui fanno prediction su imagenet
-                                                 include_preprocessing=False) #preprocessing disabilitato ma la rete si aspetta tensori con valori nel range [0-255] #TODO forse possiamo non riscalare a mano l'HOSVD?
+def get_compiled_model(input_shape, trainable_base = False, lr = 1e-3):
+    #instantiate a base model with pre-trained weight
+    base_model = keras.applications.MobileNetV3Large(input_shape = input_shape,
+                                                     weights="imagenet",
+                                                     include_top=False, #non include l'MLP con cui fanno prediction su imagenet
+                                                     include_preprocessing=False) #preprocessing disabilitato ma la rete si aspetta tensori con valori nel range [0-255] #TODO forse possiamo non riscalare a mano l'HOSVD?
 
-#freeze base model
-base_model.trainable = False
+    if trainable_base:
+        #unfreeze base model
+        base_model.trainable = True
+    else:
+        #freeze the base model
+        base_model.trainable = False
 
-#Create a new model on top of base model
-inputs = keras.Input(shape=input_shape)
+    #Create a new model on top of base model
+    inputs = keras.Input(shape=input_shape)
 
-#making sure that base model runs in inference only mode (setting training = False), important for fine-tuning
-x = base_model(inputs, training=False)
-# Convert features of shape `base_model.output_shape[1:]` to vectors
-base_model_encoding = keras.layers.GlobalAveragePooling2D()(x)
-# A Dense classifier with a single unit output (binary classification)
-dense1 = keras.layers.Dense(960, activation = "relu")
-dense2 = keras.layers.Dense(864, activation = "relu")
-dense3 = keras.layers.Dense(691, activation = "relu")
-dense4 = keras.layers.Dense(483, activation = "relu")
-dense5 = keras.layers.Dense(289, activation = "relu")
+    #making sure that base model runs in inference only mode (setting training = False), important for fine-tuning
+    x = base_model(inputs, training=False)
+    # Convert features of shape `base_model.output_shape[1:]` to vectors
+    base_model_encoding = keras.layers.GlobalAveragePooling2D()(x)
+    # A Dense classifier with a single unit output (binary classification)
+    dense1 = keras.layers.Dense(960, activation = "relu")
+    dense2 = keras.layers.Dense(864, activation = "relu")
+    dense3 = keras.layers.Dense(691, activation = "relu")
+    dense4 = keras.layers.Dense(483, activation = "relu")
+    dense5 = keras.layers.Dense(289, activation = "relu")
 
-x = dense1(base_model_encoding)
-x = dense2(x)
-x = dense3(x)
-x = dense4(x)
-x = dense5(x)
-outputs = keras.layers.Dense(1)(x)
-#outputs = keras.layers.Dense(1)(base_model_encoding)
+    x = dense1(base_model_encoding)
+    x = dense2(x)
+    x = dense3(x)
+    x = dense4(x)
+    x = dense5(x)
+    outputs = keras.layers.Dense(1)(x)
+    #outputs = keras.layers.Dense(1)(base_model_encoding)
 
-#defining model
-model = keras.Model(inputs, outputs)
-#compiling the model
-model.compile(optimizer=keras.optimizers.Adam(learning_rate = run_params["top_model_optimizer_learning_rate"]),
-              loss=keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=[keras.metrics.BinaryAccuracy()])
+    #defining model
+    model = keras.Model(inputs, outputs)
+    #compiling the model
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate = lr),
+                  loss=keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=[keras.metrics.BinaryAccuracy()])
+    return model
+
+#get frozen model
+model = get_compiled_model(input_shape, trainable_base = False, lr = run_params["top_model_optimizer_learning_rate"])
 
 print("Model summary")
 print(model.summary())
+
+if not os.path.isdir("checkpoints"):
+    os.mkdir("checkpoints")
 
 print("Training the classifier on top of MobileNetV3")
 model.fit(x = training_cores, y= training_labels,
           epochs=run_params["top_model_epochs"], batch_size = run_params["batch_size"],shuffle = True)
 
-# Unfreeze the base model
-base_model.trainable = True
 
-#recompiling the model to account for the change in trainability status of the base model
-model.compile(optimizer=keras.optimizers.Adam(run_params["full_model_optimizer_learning_rate"]),  # Very low learning rate
-              loss=keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=[keras.metrics.BinaryAccuracy()])
+model.save_weights(os.path.join("checkpoints", "MobileNetFrozen_weights"))
+keras.backend.clear_session()
+del model
+
+#get unfrozen model
+model = get_compiled_model(input_shape, trainable_base = True, lr = run_params["full_model_optimizer_learning_rate"])
+model.load_weights(os.path.join("checkpoints", "MobileNetFrozen_weights"))
 
 print("Training the full model")
 print(model.summary())
@@ -111,38 +125,35 @@ print(model.summary())
 #Warm starting 
 #defining lambda parameter (shrinking strength lambda must be in the range (0,1))
 #paper got best results for lambda = 0.2
-l = 0.6
+l = 0.8
 #defining perturbation parameters
 mu = 0
-sigma = 0.01 #see paper figure 8 for reference to select lambda and sigma parameters
+sigma = 0.05
+noise_scale = 0.0001 #see paper figure 8 for reference to select lambda and sigma parameters
 #definining number of repetitions
-spr_reps = 2
+spr_reps = run_params["spr_reps"]
 
 #excluding input and global average pooling layers from shrink perturb repeat
 excluded_layers = [0, 2]
 
 #repeat
 print("Training the full model with shrink-perturb-repeat for {} repetitions".format(spr_reps))
+#TODO questo andrebbe fatto quando ci sono più batch di immagini su cui addestrarsi
+#non ripetendo l'addestramento N volte sullo stesso batch
+#appena avremo più immagini va modificato
 for r in range(spr_reps):
     print("Shrinking and perturbing each layer's weights")
     
+    if r>0:
+        model = get_compiled_model(input_shape, trainable_base = True, lr = run_params["full_model_optimizer_learning_rate"])
+        model.load_weights(os.path.join("checkpoints", "SPR_run_{}_weights".format(r)))
+        
     for i in range(len(model.layers)):
         if i not in excluded_layers:
-            if i == 1:
-                continue
-                # layer = model.layers[i]
-                # #print("Layer: {} Name {}".format(i, layer.name))
-                # layer_weights = layer.get_weights()
-                # for j in range(len(layer_weights)):
-                #     for k in range(len(layer_weights[j])):
-                #         if not isinstance(layer_weights[j][k], np.float32):
-                #             layer_weights[j][k] = l*layer_weights[j][k] + np.random.normal(mu, sigma, size = layer_weights[j][k].shape)
-                # #set mobilenet layer's new weights
-                # layer.set_weights(layer_weights)
-            else:
+            if i != 1:
                 #print("Layer: {} Name {}".format(i, model.layers[i].name))
                 item = model.layers[i].get_weights()
-                shrinked_w = l*item[0] + np.random.normal(loc = mu, scale = sigma, size = item[0].shape)
+                shrinked_w = l*item[0] + noise_scale*np.random.normal(loc = mu, scale = sigma, size = item[0].shape)
                 shrinked_item = [shrinked_w, item[1]]
                 model.layers[i].set_weights(shrinked_item)
 
@@ -152,5 +163,11 @@ for r in range(spr_reps):
     
     print("Evaluating model on training set for iteration {}".format(r+1))
     model.evaluate(x=training_cores, y=training_labels, batch_size = run_params["batch_size"])
+        
+    model.save_weights(os.path.join("checkpoints", "SPR_run_{}_weights".format(r+1)))
+    keras.backend.clear_session()
+    del model
+    gc.collect()
+    
 #TODO salvare il modello
 
