7f4a5936c57d6acdf3b66c1534e9cc8038d917c9
mtanf <marco.tanfoni@student.unisi.it>
2023-11-17 12:34:21 +0100

    Fixed training script and json

--- NEWNEW_mobilenetClassifier.py
+++ NEWNEW_mobilenetClassifier.py
@@ -46,20 +46,55 @@ def get_classifier(encoder, num_classes=1):
     return classifier_model
 
 
-def build_model(input_shape, num_classes=1, trainable_last_layer=False):
-    encoder = get_encoder(input_shape, trainable_last_layer)
-    classifier = get_classifier(encoder, num_classes)
+def build_model(input_shape, trainable_base=False, lr=1e-3, use_pooling=False):
+    # !!!!!!
+    # MobileNetV3 models expect their inputs to be float tensors of pixels with values in the [0-255] range
+    # !!!!!!
+    # instantiate a base model with pre-trained weight
+    base_model = keras.applications.MobileNetV3Large(input_shape=input_shape,
+                                                     weights="imagenet",
+                                                     include_top=False,
+                                                     # non include l'MLP con cui fanno prediction su imagenet
+                                                     include_preprocessing=True)  # preprocessing disabilitato ma la rete si aspetta tensori con valori nel range [0-255] #TODO forse possiamo non riscalare a mano l'HOSVD?
 
-    model = keras.Model(inputs=encoder.input, outputs=classifier(encoder.output))
+    if trainable_base:
+        # unfreeze base model
+        base_model.trainable = True
+    else:
+        # freeze the base model
+        base_model.trainable = False
 
+    # Create a new model on top of base model
+    inputs = keras.Input(shape=input_shape)
+
+    # making sure that base model runs in inference only mode (setting training = False), important for fine-tuning
+    x = base_model(inputs, training=False)
+    # Convert features of shape `base_model.output_shape[1:]` to vectors
+    if use_pooling:
+        base_model_encoding = keras.layers.GlobalAveragePooling2D()(x)
+        classification_layer = keras.layers.Dense(100, activation="relu")(base_model_encoding)
+        outputs = keras.layers.Dense(1, activation="sigmoid")(classification_layer)
+    else:
+        # Flatten the features
+        x = keras.layers.Flatten()(x)
+        classification_layer = keras.layers.Dense(100, activation="relu")(x)
+        outputs = keras.layers.Dense(1, activation="sigmoid")(classification_layer)
+
+    # defining model
+    model = keras.Model(inputs, outputs)
+    # # compiling the model
+    # model.compile(optimizer=keras.optimizers.Adam(learning_rate=lr),
+    #               loss=keras.losses.BinaryCrossentropy(from_logits=False),
+    #               metrics=[keras.metrics.BinaryAccuracy()])
     return model
+
 
 
 def load_and_preprocess_image(filename, label, image_size):
     image_string = tf.io.read_file(filename)
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = tf.image.resize(image, image_size)
-    image = tf.keras.applications.mobilenet_v3.preprocess_input(image)
+    # image = tf.keras.applications.mobilenet_v3.preprocess_input(image)
     label = tf.convert_to_tensor(label)
     return image, label
 
@@ -69,10 +104,10 @@ parser.add_argument("-json", "--path_to_json", required=True, help="Path to conf
 args = vars(parser.parse_args())
 json_path = args["path_to_json"]
 
-training_max_batches = 20
+training_max_batches = None
 use_pooling = False
-use_validation_data_in_training = False
-use_data_aug = True
+use_validation_data_in_training = True
+use_data_aug = False
 
 main_model_output_path = "trained_classifiers"
 classifier_name = "MobileNetV3_frozen_useDataAug_" + str(use_data_aug)
@@ -156,6 +191,7 @@ if use_data_aug:
         seed=123
     )
 
+
 print("Loading validation images")
 val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     parent_dir_val,
@@ -182,39 +218,49 @@ test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     seed=123
 )
 
-model = build_model(model_input_shape, num_classes=1, trainable_last_layer=False)
+model = build_model(model_input_shape)
 
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=run_params["top_model_optimizer_learning_rate"]),
               loss=keras.losses.BinaryCrossentropy(from_logits=False),
               metrics=[keras.metrics.BinaryAccuracy()])
 
-idx = 1
+
 if use_data_aug:
-    for images, labels in train_dataset_aug:
-        print("Training batch {} out of {}".format(idx, len(train_dataset)))
-        if use_validation_data_in_training:
-            model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True,
-                      validation_data=val_dataset)
-        else:
-            model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True)
-        if training_max_batches is not None:
-            if idx > training_max_batches:
-                print("Breaking, max training batches reached ({})".format(training_max_batches + 1))
-                break
-        idx += 1
+    for epoch in range(run_params["training_epochs"]):
+        print("Epoch {} out of {}".format(epoch + 1, run_params["training_epochs"]))
+        for idx, (images, labels) in enumerate(train_dataset_aug):
+            print("Training batch {} out of {}".format(idx + 1, len(train_dataset_aug)))
+            if use_validation_data_in_training:
+                model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True,
+                          validation_data=val_dataset)
+            else:
+                model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True)
+            if training_max_batches is not None:
+                if idx >= training_max_batches:
+                    print("Breaking, max training batches reached ({})".format(training_max_batches))
+                    break
+            else:
+                if idx >= len(train_dataset_aug):
+                    # print("Breaking, max training batches reached ({})".format(len(train_dataset_aug)))
+                    break
 else:
-    for images, labels in train_dataset:
-        print("Training batch {} out of {}".format(idx, len(train_dataset)))
-        if use_validation_data_in_training:
-            model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True,
-                      validation_data=val_dataset)
-        else:
-            model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True)
-        if training_max_batches is not None:
-            if idx > training_max_batches:
-                print("Breaking, max training batches reached ({})".format(training_max_batches + 1))
-                break
-        idx += 1
+    for epoch in range(run_params["training_epochs"]):
+        print("Epoch {} out of {}".format(epoch + 1, run_params["training_epochs"]))
+        for idx, (images, labels) in enumerate(train_dataset):
+            print("Training batch {} out of {}".format(idx + 1, len(train_dataset)))
+            if use_validation_data_in_training:
+                model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True,
+                          validation_data=val_dataset)
+            else:
+                model.fit(x=images, y=labels, epochs=run_params["NoFineTuning_BatchEpochs"], shuffle=True)
+            if training_max_batches is not None:
+                if idx >= training_max_batches:
+                    print("Breaking, max training batches reached ({})".format(training_max_batches))
+                    break
+            else:
+                if idx >= len(train_dataset):
+                    # print("Breaking, max training batches reached ({})".format(len(train_dataset_aug)))
+                    break
 
 print("Saving model")
 model.save(os.path.join(weight_output_path, classifier_name + "_weights.h5"))
@@ -280,7 +326,7 @@ class_names = ["real", "fake"]
 
 explainer = shap.Explainer(model, masker, output_names=class_names)
 
-shap_values = explainer(X_shap[:num_images_shap], outputs=shap.Explanation.argsort.flip[:num_images_shap])
+shap_values = explainer(X_shap[:num_images_shap], outputs=shap.Explanation.argsort.flip[:num_images_shap], max_evals=1000)
 
 mapping = dict(zip([0, 1], class_names))
 
